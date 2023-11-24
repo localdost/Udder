8d20ec2773a61b5d3638d30138ee9201ae93fa36
Cem <cemabdullahsavkli@gmail.com>
2023-11-24 12:55:00 +0300

    add disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,5 +17,8 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         <#code#>
     }
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
